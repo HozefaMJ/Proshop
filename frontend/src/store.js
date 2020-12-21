5837982b9f9403dbd9970572f7f32e9be0053a98
Hozefa <hozefa24imp@gmail.com>
2020-12-21 17:15:23 +0530

    FE Profile Screen GET and Update

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,7 +7,7 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 // Reducers
 import {productListReducer, productDetailsReducer} from './reducers/productReducers';
 import {cartReducer} from "./reducers/cartReducers";
-import {userLoginReducer, userRegisterReducer} from "./reducers/userReducers";
+import {userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer} from "./reducers/userReducers";
 
 
 const reducer = combineReducers({
@@ -15,7 +15,9 @@ const reducer = combineReducers({
     productDetails: productDetailsReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
-    userRegister: userRegisterReducer
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
