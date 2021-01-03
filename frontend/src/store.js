6fee99ba61926488da0fe7a9965cb992f966b1cd
Hozefa <hozefa24imp@gmail.com>
2021-01-03 13:04:00 +0530

    FE Admins getUserList and AdminScreen with constants reducers actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,7 +7,7 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 // Reducers
 import {productListReducer, productDetailsReducer} from './reducers/productReducers';
 import {cartReducer} from "./reducers/cartReducers";
-import {userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer} from "./reducers/userReducers";
+import {userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer, userListReducer} from "./reducers/userReducers";
 import {orderCreateReducer, orderDetailsReducer, orderListMyReducer, orderPayReducer} from "./reducers/orderReducers";
 
 const reducer = combineReducers({
@@ -18,10 +18,12 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
+    userList: userListReducer,
     orderCreate:  orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
     orderMyList: orderListMyReducer
+
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
